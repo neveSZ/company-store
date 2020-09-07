031fec72a4ea1f5148936ff9dac733883363f63e
neveSZ <vnsilva2000@gmail.com>
2020-09-07 03:03:25 -0300

    WIP: Colaborador consegue visualizar todos os produtos ofertados por pessoas da mesma empresa

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
   def index
     @categories = Category.all
     @collaborator = current_collaborator
+    @products = Product.joins(:collaborator).where(status: 0, collaborators: { company: @collaborator.company })
   end
 end
