4dc23ae3e8f607501f9911195e5107280caef126
neveSZ <vnsilva2000@gmail.com>
2020-09-07 03:09:28 -0300

    Adicionado cenario de teste se não houver produto

--- spec/features/collaborator_see_all_products_from_same_company_spec.rb
+++ spec/features/collaborator_see_all_products_from_same_company_spec.rb
@@ -6,7 +6,6 @@ feature 'collaborator can see all products from his company' do
 
     collaborator_main = Collaborator.create!(email: 'victor@treinadev.com.br', password: '!a123456789b', full_name: 'Victor Neves Silva', birth_date: '13/07/2000', role: 'Analista', department: 'Tecnologia')
     collaborator_secundary = Collaborator.create!(email: 'victor@empresa.com.br', password: '!a123456789b', full_name: 'Victor Neves Silva', birth_date: '13/07/2000', role: 'Analista', department: 'Tecnologia')
-    images = ['spec/files/images/1.jpg', 'spec/files/images/2.jpg', 'spec/files/images/3.jpg']
     Product.create!(name: 'S20', category: category, description: 'teste teste', value: 10.00, status: 0, collaborator: collaborator_main)
     Product.create!(name: 'S30', category: category, description: 'teste teste', value: 100.00, status: 0, collaborator: collaborator_main)
     Product.create!(name: 'S40', category: category, description: 'teste teste', value: 200.00, status: 1, collaborator: collaborator_main)
@@ -16,6 +15,7 @@ feature 'collaborator can see all products from his company' do
 
     login_as collaborator_main
     visit root_path
+
     expect(page).to have_content('S20')
     expect(page).to have_content('R$ 10,00')
     expect(page).to have_content('S30')
@@ -27,5 +27,27 @@ feature 'collaborator can see all products from his company' do
     expect(page).to have_content('Comprar', count: 2)
   end
 
-  
+  scenario 'no product from his company' do
+    category = Category.create!(name: 'Celulares', description: 'ASDIKPOSDKOPKOPADKOPASD')
+
+    collaborator_main = Collaborator.create!(email: 'victor@treinadev.com.br', password: '!a123456789b', full_name: 'Victor Neves Silva', birth_date: '13/07/2000', role: 'Analista', department: 'Tecnologia')
+    collaborator_secundary = Collaborator.create!(email: 'victor@empresa.com.br', password: '!a123456789b', full_name: 'Victor Neves Silva', birth_date: '13/07/2000', role: 'Analista', department: 'Tecnologia')
+    Product.create!(name: 'S20', category: category, description: 'teste teste', value: 10.00, status: 0, collaborator: collaborator_secundary)
+    Product.create!(name: 'S30', category: category, description: 'teste teste', value: 100.00, status: 0, collaborator: collaborator_secundary)
+    Product.create!(name: 'S40', category: category, description: 'teste teste', value: 200.00, status: 1, collaborator: collaborator_secundary)
+    Product.create!(name: 'S50', category: category, description: 'teste teste', value: 300.00, status: 2, collaborator: collaborator_secundary)
+    Product.create!(name: 'S60', category: category, description: 'teste teste', value: 400.00, status: 3, collaborator: collaborator_secundary)
+    Product.create!(name: 'S70', category: category, description: 'teste teste', value: 400.00, status: 0, collaborator: collaborator_secundary)
+
+    login_as collaborator_main
+    visit root_path
+
+    expect(page).to have_content('Nenhum produto disponível')
+    expect(page).to_not have_content('S20')
+    expect(page).to_not have_content('S30')
+    expect(page).to_not have_content('S40')
+    expect(page).to_not have_content('S50')
+    expect(page).to_not have_content('S60')
+    expect(page).to_not have_content('S70')
+  end
 end
