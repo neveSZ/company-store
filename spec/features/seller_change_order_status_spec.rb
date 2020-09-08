89f9d63e35e0560350de18b40f0ea665ff004641
neveSZ <vnsilva2000@gmail.com>
2020-09-08 11:33:45 -0300

    Sumir com os botões de concluir/comprar venda após mudar o status do pedido

--- spec/features/seller_change_order_status_spec.rb
+++ spec/features/seller_change_order_status_spec.rb
@@ -73,4 +73,21 @@ feature 'Seller change order status' do
 
     expect(page).to have_content('S20')
   end
+
+  scenario 'hide buttons after changed' do
+    category = Category.create!(name: 'Celulares', description: 'teste')
+    seller = Collaborator.create!(email: 'victor@treinadev.com.br', password: '!a123456789b', full_name: 'Victor Neves Silva', birth_date: '13/07/2000', role: 'Analista', department: 'Tecnologia')
+    buyer = Collaborator.create!(email: 'joao@treinadev.com.br', password: '!a123456789b', full_name: 'Joao Neves Silva', birth_date: '13/07/2000', role: 'Analista', department: 'TI')
+    product = Product.create!(name: 'S20', category: category, description: 'teste teste', value: 10.00, collaborator: seller)
+    order = Order.create!(collaborator: buyer, product: product)
+
+    login_as seller
+    visit root_path
+    click_on 'Minhas vendas'
+    click_on "Pedido ##{order.id} - #{product.name}"
+    click_on 'Cancelar venda'
+
+    expect(page).to_not have_content('Cancelar venda')
+    expect(page).to_not have_content('Concluir venda')
+  end
 end
